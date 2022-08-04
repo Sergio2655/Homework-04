1b20fb85fd39f01f0f8743a2b2bbaeaa991d8235
Sergey <sp27101971@gmail.com>
2022-08-04 17:24:16 +0300

    Make Array

---
+++ Task-29/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] myArrray = new int[8];
+Random point = new Random();
+for(int i = 0; i < myArrray.Length; i++)
+{
+    myArrray[i] = point.Next(0,8);
+}
+for(int i = 0; i < myArrray.Length; i++)
+{
+    Console.Write(myArrray[i] + " ");
+
+}
